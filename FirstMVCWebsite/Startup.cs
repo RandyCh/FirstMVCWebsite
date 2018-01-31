1bebc2e11c9b0c928f462c8179d11a707236929a
RandyCheng <linusche@gmail.com>
2018-01-31 11:34:38 +0800

    加入專案檔案。

---
+++ FirstMVCWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstMVCWebsite.Startup))]
+namespace FirstMVCWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
